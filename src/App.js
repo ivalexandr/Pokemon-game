f8c5a5762c0f7a64026dc103f55e67ff2bacd5be
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-01-27 19:43:03 +0300

    Complete task from homework_2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Components/Header/Header'
+import Layout from './Components/Layout/Layout'
+import Footer from './Components/Footer/Footer'
+import img from './bg1.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world, REACT!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header
+        title='This is title'
+        descr='This is description'
+      />
+      <Layout
+        id='roots'
+        title = 'this is title in Layout'
+        descr = 'this is descr in Layout'
+        urlBg = {img}
+      />
+      <Layout
+        id='about'
+        title = 'this is title in Layout'
+        descr = 'this is descr in Layout'
+        colorBg = '#e6e6e6'
+      />
+      <Layout
+        id='contacts'
+        title = 'this is title in Layout'
+        descr = 'this is descr in Layout'
+        urlBg = {img}
+      />
+      <Footer />
+    </>
   );
 }
 
-export default App;
+export default App
