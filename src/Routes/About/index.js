2005ae0386d6cabc152ed62fb4c09407a74e9fb1
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-02-06 14:36:59 +0300

    complete homework_6

--- src/Routes/About/index.js
+++ src/Routes/About/index.js
@@ -1,6 +1,3 @@
-import MenuHeader from '../../Components/MenuHeader/MenuHeader'
-import classes from './index.module.css'
-
 const AboutPage = () => {
 
     return(
--- src/Routes/Game/index.js
+++ src/Routes/Game/index.js
@@ -1,42 +1,84 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import database from '../../services/firebase'
 import Layout from '../../Components/Layout/Layout'
-import { useHistory } from 'react-router-dom'
-import { POKEMONS } from '../../Pokemon'
 import PokemonCard from '../../Components/PokemonCard/PokemonCard'
 import img from '../../Routes/Home/bg1.jpg'
 import classes from './style.module.css'
 const GamePage = () => {
-    const history = useHistory()
-    const handlerClick = () => {
-        history.push('/')
-    }
-    const [isCards, setCards] = useState(POKEMONS)
+    const [isCards, setCards] = useState({})
     const handlerClickCard = (id) => {
-        let newPokemonsCards = []
-        newPokemonsCards = isCards.map(item => {
-            if (item.id === id) {
-                return {
-                    ...item,
-                    active: !item.active
+        setCards(prevState => {
+            return Object.entries(prevState).reduce((acc, item) => {
+                const pokemon = { ...item[1] }
+                if (pokemon.id === id) {
+                    pokemon.active = !pokemon.active
                 }
-            }
-            return item
+                acc[item[0]] = pokemon
+                return acc
+            }, {})
         })
-        setCards(newPokemonsCards)
+        Object.entries(isCards).forEach(([key, item]) => {
+            const pokemon = { ...item }
+            if (pokemon.id === id) {
+                database.ref('pokemons/' + key).set(({
+                    ...pokemon,
+                    active: !pokemon.active,
+                }))
+            }
+        })
     }
+    const handlerClickAdd = () => {
+        const newKey = database.ref().child('pokemons').push().key;
+        database.ref('pokemons/' + newKey).set({
+            "abilities": ["keen-eye", "tangled-feet", "big-pecks"],
+            "base_experience": 122,
+            "height": 11,
+            "id": 17,
+            "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
+            "name": "pidgeotto",
+            "stats": {
+                "attack": 60,
+                "defense": 55,
+                "hp": 63,
+                "special-attack": 50,
+                "special-defense": 50,
+                "speed": 71
+            },
+            "type": "flying",
+            "values": {
+                "bottom": 7,
+                "left": 5,
+                "right": 2,
+                "top": "A"
+            }
+        })
+        setCards(prevState => {
+            return Object.entries(prevState).reduce((acc, item) => {
+                const pokemon = { ...item[1] }
+                acc[item[0]] = pokemon
+                return acc
+            }, {})
+        })
+    }
+    useEffect(() => {
+        database.ref('pokemons').once('value', snapshot => {
+            setCards(snapshot.val())
+        })
+    }, [isCards])
+
     return (
         <>
-            <button className={classes.btn} onClick={handlerClick}>
-                Return to HomePage
-            </button>
             <Layout
                 title="CARDS GAME POKEMONS!"
-                urlBg = {img}
+                urlBg={img}
             >
+                <button className={classes.btn} onClick={handlerClickAdd}>
+                    ADD CARD
+            </button>
                 <div className={classes.flex}>
                     {
-                        isCards.map(item => <PokemonCard
-                            key={item.id}
+                        Object.entries(isCards).map(([key, item]) => <PokemonCard
+                            key={key}
                             name={item.name}
                             img={item.img}
                             id={item.id}
